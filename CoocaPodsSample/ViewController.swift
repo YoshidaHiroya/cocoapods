112b2e2bd42fe17e326d33d47f978dc23f1b4825
YoshidaHiroya <hdotm1225@gmail.com>
2018-10-16 18:03:49 +0900

    first commit

--- CoocaPodsSample/ViewController.swift
+++ CoocaPodsSample/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SVProgressHUD
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showHUD(){
+        SVProgressHUD.show()
+    }
+    @IBAction func dismissHUD(){
+        SVProgressHUD.dismiss()
+    }
 }
 
